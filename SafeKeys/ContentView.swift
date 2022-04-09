6e41aa39a5cf07941ce4d0e373e78fa4604e18f8
Verones Lorenzo <veroneslorenzo@gmail.com>
2022-04-09 08:10:32 +0200

    Initial Commit

---
+++ SafeKeys/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SafeKeys
+//
+//  Created by Lorenzo Verones on 09/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SafeKeys/SafeKeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeKeysApp.swift
+//  SafeKeys
+//
+//  Created by Lorenzo Verones on 09/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SafeKeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
